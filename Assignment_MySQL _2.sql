a8ffebf93eebd5e511ca61a9f2aa86adb5085807
RVM41 <rashbsf@gmail.com>
2024-10-12 16:53:20 +0400

    Add files via upload

--- Assignment_MySQL _2.sql
+++ Assignment_MySQL _2.sql
@@ -10,8 +10,25 @@ Create a database called “Sales” and create a new table named “Orders” i
  use sales;
  Create table Orders(Order_ID int primary key,
  Costomer_Name varchar(20) not null,
- Product_Category varchar(20) not null Unique,
+ Product_Category varchar(20) not null ,
  Ordered_item varchar(20),
  Contact_No Varchar(10) not null unique);
  desc orders;
-
+ 
+/* 1. Add a new column named “order_quantity” to the
+ orders table*/
+alter table orders add column order_quantity int ;
+set sql_safe_updates=0;
+alter table sales_orders drop column Product_Category;
+alter table sales_orders add column  Product_category varchar(20) not null;
+## 2. Rename the orders table to the sales_orders table.
+rename table orders to Sales_Orders;
+desc sales_orders;
+## 3. Insert 10 rows into the sales_orders table.
+insert into sales_orders(Order_ID,Costomer_Name,Product_Category,Ordered_item,Contact_No,order_quantity) values (1001,"Gopika","Electronics","Fan",
+"9847363476",23) ,(1002,"Shekar","Toys","Remote car","9023457654",30) ,(1003,"Ahmed","Electronics","Torch light","9832763490",32) ,(1004,
+"Sujadha","Home Appliance","Gas stove","9045876521",12) ,(1005,"Hariharan","Electronics","Led bulb","9087123465",35),
+(1006,"Ahmed","Electronics","Emergency Lamb","9787676532",10) ,(1007,"Hajara","Toys","Helicopter","9009897856",22) ,(1008,"Ashokan","Toys","Doll","9190983421",10),
+(1009,"Sajna","Home Appliance","Gas stove Liter","9190948744",20) ,(1010,"Hariharan","Electronics","Reachargeble Bulb","9645234578",25);
+drop data
+drop database sales;
